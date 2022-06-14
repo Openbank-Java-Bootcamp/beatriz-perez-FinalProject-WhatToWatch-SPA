0639c226a9a3eb918487e7dd85bf67963bee850b
beatriz-perez <beatriz.pfp@gmail.com>
2022-06-14 22:55:10 +0200

    Add set item as watched feature

--- src/pages/Detail/WatchItemPage.js
+++ src/pages/Detail/WatchItemPage.js
@@ -2,6 +2,7 @@
 import { useParams } from "react-router-dom";
 import { useContext, useState, useEffect } from "react";
 import { SourceContext } from "../../context/source.context";
+import { AuthContext } from "../../context/auth.context";
 import axios from "axios";
 
 // Components:
@@ -17,20 +18,45 @@ function WatchItemPage() {
     const { id:itemId } = useParams();
     const [item, setItem] = useState(null);
 
+    const { user:logedinUser } = useContext(AuthContext);
+    const [isWatcher, setIsWatcher] = useState(false);
+    const [editing, setEditing] = useState(false);
+
+
     useEffect(() => {
         getItem();
-    }, [itemId]);
+    }, [itemId, editing]);
 
     // Get Item from WTW DB
     const getItem = () => {
         const storedToken = localStorage.getItem("authToken");
         axios
             .get(`${API_URL}/api/items/${itemId}`, { headers: { Authorization: `Bearer ${storedToken}` }, })
-            .then((response) => setItem(response.data))
+            .then((response) => {
+                setItem(response.data)
+                setIsWatcher(response.data.watchers.filter(i => i.id === logedinUser.id).length > 0);
+            })
             .catch((error) => console.log(error));
     };
+
+    // Watch item (set as watched)
+    const addToWatchers = () => {
+        setEditing(true);
+        const requestBody = { id: logedinUser.id, };
+        const storedToken = localStorage.getItem("authToken");
+        const url = `${API_URL}/api/items/watch/${itemId}`;
+        axios
+            .patch(url, requestBody, { headers: { Authorization: `Bearer ${storedToken}` }, })
+            .then((response) => {
+                console.log(response.data);
+                setEditing(false);
+            })
+            .catch((error) => console.log(error.response.data.message));
+    }
+    
     
     console.log("DETAILS for: ", item);
+    console.log(isWatcher);
     return (
         <GeneralLayout >
             {item ? (
@@ -41,6 +67,11 @@ function WatchItemPage() {
                         image={item.banner}
                     />
                     <PaddingSection>
+                        {
+                            !isWatcher 
+                            ? <button onClick={addToWatchers}>Set as watched</button>
+                            : <p>WATCHED!</p>
+                        }
                         <AddItemToList  item={item} />
                     </PaddingSection>
                     <PaddingSection>
@@ -51,6 +82,7 @@ function WatchItemPage() {
                             <p>{`rating: ${item.rating}`}</p>
                             <p>{`title: ${item.title}`}</p>
                             <p>{`synopsis: ${item.synopsis}`}</p>
+                            <p>{`watchers: ${item.watchers.length}`}</p>
                         </div>
                     </PaddingSection>
                     <PaddingSection>
